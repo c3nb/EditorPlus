f545322e925ee22f5f396db0e7ff1d6a0b4950d7
C#Newbie <dongsu45681111@gmail.com>
2024-02-10 14:43:18 +0900

    초기 커밋.

---
+++ EditorPlus/Main.cs
@@ -0,0 +1,36 @@
+﻿using HarmonyLib;
+using System.Reflection;
+using static UnityModManagerNet.UnityModManager;
+using static UnityModManagerNet.UnityModManager.ModEntry;
+
+namespace EditorPlus
+{
+    public static class Main
+    {
+        public static ModEntry Mod { get; private set; }
+        public static ModLogger Logger { get; private set; }
+        public static Harmony Harmony { get; private set; }
+        public static Assembly Ass { get; private set; }
+        public static void Load(ModEntry modEntry)
+        {
+            Mod = modEntry;
+            Logger = modEntry.Logger;
+            Ass = Assembly.GetExecutingAssembly();
+            modEntry.OnToggle = OnToggle;
+        }
+        public static bool OnToggle(ModEntry modEntry, bool toggle)
+        {
+            if (toggle)
+            {
+                Harmony = new Harmony(modEntry.Info.Id);
+                Harmony.PatchAll(Ass);
+            }
+            else
+            {
+                Harmony.UnpatchAll(Harmony.Id);
+                Harmony = null;
+            }
+            return true;
+        }
+    }
+}
